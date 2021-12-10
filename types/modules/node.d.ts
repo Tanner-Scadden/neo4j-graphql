a5f6961fd885c94df65fe059ae5fd2e03581572a
Tanner Scadden <tanner@scaddenfamily.com>
2021-12-09 17:09:27 -0700

    Pushing up typescript resolution for lastRan

--- types/modules/node.d.ts
+++ types/modules/node.d.ts
@@ -1,10 +1,12 @@
-import type { Driver } from "neo4j-driver";
+import type { Driver } from 'neo4j-driver';
 
 declare global {
   // eslint-disable-next-line
   var neo4j: {
     driver?: Driver;
   };
+  // USED FOR DEVELOPING TYPES AUTOMATICALLY IN DEV
+  var lastRan: Date;
 }
 
 export default global;
